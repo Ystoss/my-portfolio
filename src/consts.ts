abdaa9e8f65e9f84171912762caf3e86b0487707
Jules Abran <julesabran@gmail.com>
2026-01-28 12:40:51 +0100

    first commit

---
+++ src/consts.ts
@@ -0,0 +1,27 @@
+export const SITE_TITLE = "Jules Abran";
+export const SITE_DESCRIPTION =
+  `Jules Abran's Gameplay Programmer portfolio`.trim();
+
+export const KNOWN_TECH =
+  `Unity, Godot, C#, Blender, Git, Game UX, Game Feel, Juiciness, Gameplay Systems, Physics-based Gameplay`.split(
+    ",",
+  );
+export const ABOUT_ME =
+  `I am a Gameplay Programmer, currently in my 2nd year of studying at ISART Digital in Paris. My passion for video games started in childhood, I still remember realizing that you could cry tears of joy when I received my very first console at the age of 7: a Wii. 
+  
+  I am also passionate about science and mathematics and love logic and problem solving. That's why it is my dream to work in video games as a Gameplay Programmer, uniting both passion and skills I'm good at.`.trim();
+export const GITHUB_USERNAME = "Ystoss";
+export const QUOTE = "Gameplay Programmer studying at ISART Digital";
+export const NAV_LINKS: Array<{ title: string; href?: string }> = [
+  {
+    title: "Games",
+  },
+  {
+    title: "Linkedin",
+    href: "//linkedin.com/in/jules-abran-638689328/",
+  },
+  {
+    title: "Resume",
+    href: "files/ABRAN_Jules_CV.pdf",
+  },
+];
---
+++ src/content.config.ts
@@ -0,0 +1,19 @@
+import { glob } from "astro/loaders";
+import { defineCollection, z } from "astro:content";
+
+const games = defineCollection({
+  // Load Markdown and MDX files in the `src/content/blog/` directory.
+  loader: glob({ base: "./src/content/games", pattern: "**/*.{md,mdx}" }),
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    gametype: z.string(),
+    order: z.number(),
+    heroVideo: z.string().optional(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional(),
+  }),
+});
+
+export const collections = { games };
